d31dbc7d8c738feb89b23d863cd85679853dbda0
cronalds <124749667+cronalds@users.noreply.github.com>
2024-06-21 20:15:45 +1000

    first commit; the most basic setup

---
+++ README.md
@@ -0,0 +1,7 @@
+a css library written in sass; will be similar to bootstrap but rather different; I may also perform petty theft on bootstrap; I don't know.
+
+tech:
+- sass
+- gulp
+- gulp-sass
+- bootstrap?
---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+import { src, dest, watch, series } from "gulp";
+import gulpSass from "gulp-sass";
+import * as sass from "sass";
+const s = gulpSass(sass);
+
+function buildStyles() {
+  return src("./scss/main.scss")
+    .pipe(s().on("error", s.logError))
+    .pipe(dest("./css"));
+}
+
+function watchTask() {
+  watch("./scss/**/*.scss", buildStyles);
+  return Promise.resolve();
+}
+
+export default series(buildStyles, watchTask);
